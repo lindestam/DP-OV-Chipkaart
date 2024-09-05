6312f2b6b2498091ed957ae9c95ce688d9a79c8a
linde <linde.stam@student.hu.nl>
2024-09-05 14:43:08 +0200

    linde

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
